5528c23e3267d792fc2b404ff90221f351d5ec80
Ankita Chavan <ankitac@apporchid.com>
2020-08-29 21:05:34 +0530

    banner added

--- src/app/home-page/night-owler/night-owler.component.ts
+++ src/app/home-page/night-owler/night-owler.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./night-owler.component.scss']
 })
 export class NightOwlerComponent implements OnInit {
-
+  content = {
+    "bgImage":'nightOwler',
+    "title": 'The Nightowlers',
+    'subTitle': 'One Stop Solution for all your freelancing needs. We are revolutionalizing how Freelancing works in India.',
+    "subQuestion": 'Wanna Know How ?',
+    "button": [
+      {
+        "title" : 'Visit Website',
+        "style": 'btn-primaryOrange'
+      },
+      {
+        "title" : 'Read More',
+        "style": 'blur-btn'
+      }
+    ]
+  };
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.content,"sending");
+    
   }
 
 }
--- src/app/shared/component/banner/banner.component.ts
+++ src/app/shared/component/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-
+@Input() content;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.content,"content");
+    
   }
 
 }
