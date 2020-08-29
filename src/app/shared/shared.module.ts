bc17ce3d3f48c76b59d33e7aca0113627bc8c5c5
Ankita Chavan <ankitac@apporchid.com>
2020-08-29 15:50:12 +0530

    added nightowler

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CardComponent} from './component/card/card.component';
 import { HeaderComponent} from './component/header/header.component';
+import { BannerComponent } from './component/banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [CardComponent, HeaderComponent],
+  declarations: [CardComponent, HeaderComponent, BannerComponent],
   imports: [
     CommonModule
   ],
-  exports: [CardComponent, HeaderComponent]
+  exports: [CardComponent, HeaderComponent,BannerComponent]
 })
 export class SharedModule { }
