369bcf2d16bf1d730fd7f1efe376f42a9d5da605
Ankita Chavan <ankitac@apporchid.com>
2020-08-29 23:18:35 +0530

    added cards

--- src/app/home-page/ecommerce/ecommerce.component.ts
+++ src/app/home-page/ecommerce/ecommerce.component.ts
@@ -7,20 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EcommerceComponent implements OnInit {
   content = {
-    "bgImage":'ecommerce',
+    "bgImage": 'ecommerce',
     "title": 'Multi-Vendor Ecommerce',
     'subTitle': 'Plug & Play Multi-Vendor online store with 24*7 Tech Support. So, you can focus on business while we take care of your store.',
     "getDetailsForm": true,
     "button": [
       {
-        "title" : 'Read More',
+        "title": 'Read More',
         "style": 'blur-btn'
       }
     ]
   };
+  cardContent = [{
+    "title": 'Administered Order Management System',
+    "paragraph": 'If you do not want to directly map your customer orders to the vendors and select a vendor to assign a specific item out of the list of vendors who can carter the order. We have this feature covered for you.'
+  }, {
+    "title": 'Service area wise product catalogue and pricing management',
+    "paragraph": 'Create your own virtual geography using pincodes while defining'
+  }, {
+    "title": 'Mobile friendly Website and Mobile Application for your Buyers',
+    "paragraph": 'Create your own virtual geography using pincodes while defining'
+  }, {
+    "title": 'Cross-Platform Mobile App for Vendors to maintain product catalogues and orders',
+    "paragraph": 'Create your own virtual geography using pincodes while defining'
+  }]
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.cardContent,"cardContent");
+    
   }
 
 }
