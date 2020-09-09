ad64beb77e1b1f67f01478dd904c116852094e65
Ankita Chavan <ankitac@apporchid.com>
2020-09-10 01:16:34 +0530

    added footer

--- src/app/shared/component/footer/footer.component.ts
+++ src/app/shared/component/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+email: any;
+number:any;
+  constructor(private router: Router,private viewportScroller: ViewportScroller, ) { }
 
   ngOnInit(): void {
   }
-
+  onClick(path: string,elementid?){
+    if(elementid){
+      this.viewportScroller.scrollToAnchor(path);
+    }
+    this.router.navigate([]).then(result => {  window.open(path, '_blank'); });
+  }
+  submit(){}
 }
