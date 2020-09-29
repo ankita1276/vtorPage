adb972f96d6c19f17c3f312b2edeec793bfe7872
Ankita Chavan <ankitac@apporchid.com>
2020-09-29 19:16:14 +0530

    added chnages

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,52 +8,50 @@ import { ComponentService } from '../shared/service/component.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  email:any;
-  SelectedQuery:any;
-  startIndex: number=0;
-  isErrors:boolean=false;
-  static MessageContent:String;
-  public homePageComponent=HomePageComponent;
-  constructor(private router: Router,private service: ComponentService) { }
+  email: any;
+  SelectedQuery: any;
+  startIndex: number = 0;
+  isErrors: boolean = false;
+  static MessageContent: String;
+  public homePageComponent = HomePageComponent;
+  constructor(private router: Router, private service: ComponentService) { }
 
   ngOnInit(): void {
-    console.log(this.router.url); 
+    console.log(this.router.url);
     this.service.setShowHeader(true);
-    this.email=null;
+    this.email = null;
     this.setEnquiryType('Select Enquiry type');
-this.isErrors=false;
-   
+    this.isErrors = false;
+
   }
-  setEnquiryType(type:string)
-  {
+  setEnquiryType(type: string) {
     console.log(type)
-     this.SelectedQuery=type;
+    this.SelectedQuery = type;
   }
-  submit(){
-    if(this.email!=null && this.SelectedQuery!=='Select Enquiry type')
-    {
-    this.service.sendPostRequest('?DateTime='+new Date().toJSON("yyyy/MM/dd__HH:mm")+'&Email='+this.email+'&EnquiryType='+this.SelectedQuery);
-  HomePageComponent.setMessageContent("Thank You For Reaching Us !! Our Representative Will Contact You Within 24 Hrs");
-  document.getElementById("content").style.color='green';
-  document.getElementById("myModal").style.display='block';
-  this.delay(3000).then(any=>{
-    this.closeModal();
-   this.ngOnInit(); 
-});
-  }
-else{
-         HomePageComponent.setMessageContent("Error : Please Fill All The Details Properly This Would Help In Clear Communication.");
-         this.isErrors=true;
-         document.getElementById("content").style.color='red';
-         document.getElementById("myModal").style.display='block';
-         this.delay(3000).then(any=>{
-          this.closeModal();
-        
+  submit() {
+    if (this.email != null && this.SelectedQuery !== 'Select Enquiry type') {
+      this.service.sendPostRequest('?DateTime=' + new Date().toJSON("yyyy/MM/dd__HH:mm") + '&Email=' + this.email + '&EnquiryType=' + this.SelectedQuery);
+      HomePageComponent.setMessageContent("Thank You For Reaching Us !! Our Representative Will Contact You Within 24 Hrs");
+      document.getElementById("content").style.color = 'green';
+      document.getElementById("myModal").style.display = 'block';
+      this.delay(3000).then(any => {
+        this.closeModal();
+        this.ngOnInit();
       });
     }
-  
-    
-  
+    else {
+      HomePageComponent.setMessageContent("Error : Please Fill All The Details Properly This Would Help In Clear Communication.");
+      this.isErrors = true;
+      document.getElementById("content").style.color = 'red';
+      document.getElementById("myModal").style.display = 'block';
+      this.delay(3000).then(any => {
+        this.closeModal();
+
+      });
+    }
+
+
+
 
   }
   private newMethod() {
@@ -61,23 +59,21 @@ else{
   }
 
   async delay(ms: number) {
-    await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+    await new Promise(resolve => setTimeout(() => resolve(), ms)).then(() => console.log("fired"));
+  }
+  closeModal() {
+    document.getElementById("myModal").style.display = 'none';
+  }
+  btnRoute(path) {
+    this.router.navigate([]).then(result => { window.open(path, '_self'); });
+  }
+  static setMessageContent(data: String) {
+    this.MessageContent = data;
+  }
+
+
 }
-  closeModal()
-  {
-    document.getElementById("myModal").style.display='none';
-  }
-  btnRoute(path){
-    this.router.navigate([]).then(result => {  window.open(path, '_self'); });
-  }
-  static  setMessageContent(data:String)
-  {
-    this.MessageContent=data;
-  }
- 
-  
-  }
 
 
-  
+
 
